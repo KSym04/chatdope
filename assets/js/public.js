0a907979542ef9639ff7cdb41dd285e2f440629d
ksym04 <dev@dopethemes.com>
2023-08-07 17:07:14 +0800

    Update: Added FED interactivity.

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,29 +1,83 @@
 /**
- * This script handles the frontend chat interface for ChatDope.
- * It manages click events for contacts and the send button, fetches chat messages, and sends messages.
- *
- * @file public.js
+ * Initializes the chat input behavior.
+ * Resizes the input field based on its content, handles the Enter key to send messages, and appends messages to the chat container.
+ * @function
  */
-
 document.addEventListener("DOMContentLoaded", function () {
-  let textArea = document.getElementById("chatdope-input");
-  let initialHeight = textArea.offsetHeight; // Get the original height
-  let singleLineHeight = textArea.clientHeight; // Single line height without padding
+  /**
+   * Reference to the chat input textarea element.
+   * @type {HTMLTextAreaElement}
+   */
+  const textArea = document.getElementById("chatdope-input");
 
-  textArea.style.overflowY = "hidden"; // Hide overflow initially
+  /**
+   * Reference to the chat messages container element.
+   * @type {HTMLElement}
+   */
+  const chatContainer = document.getElementById("chatdope-chats");
 
+  /**
+   * Original height of the textarea when the page is loaded.
+   * @type {number}
+   */
+  const initialHeight = textArea.offsetHeight;
+
+  /**
+   * Height of a single line within the textarea, excluding padding.
+   * @type {number}
+   */
+  const singleLineHeight = textArea.clientHeight;
+
+  // Hide overflow initially
+  textArea.style.overflowY = "hidden";
+
+  /**
+   * Event handler for text input within the textarea.
+   * Adjusts the textarea's height based on its content.
+   */
   textArea.addEventListener("input", function () {
+    // Reset to auto height for proper calculation
     this.style.height = "auto";
 
+    // Handle case where input is blank
     if (this.value.trim() === "") {
-      this.style.height = initialHeight + "px"; // Reset to original height if blank
+      this.style.height = initialHeight + "px";
+      this.style.overflowY = "hidden";
       return;
     }
 
-    if (this.scrollHeight > singleLineHeight) {
-      this.style.height = this.scrollHeight + "px"; // Adjust height to fit content
+    // Resize textarea based on content and max height (5 lines)
+    if (
+      this.scrollHeight > singleLineHeight &&
+      this.scrollHeight <= singleLineHeight * 5
+    ) {
+      this.style.height = this.scrollHeight + "px";
+    } else if (this.scrollHeight > singleLineHeight * 5) {
+      this.style.height = singleLineHeight * 5 + "px";
+      this.style.overflowY = "auto";
     } else {
-      this.style.height = initialHeight + "px"; // Keep original height if one line
+      this.style.height = initialHeight + "px";
+    }
+  });
+
+  /**
+   * Event handler for the Enter key within the textarea.
+   * Appends messages to the chat container and clears the input.
+   */
+  textArea.addEventListener("keyup", function (e) {
+    if (e.key === "Enter" && this.value.trim() !== "") {
+      e.preventDefault();
+
+      // Create and append the message to the chat container
+      const messageDiv = document.createElement("div");
+      messageDiv.className = "chat-message sender";
+      messageDiv.textContent = this.value.trim();
+      chatContainer.insertBefore(messageDiv, chatContainer.firstChild); // Insert at the beginning
+
+      // Clear the input and reset to original height
+      this.value = "";
+      this.style.height = initialHeight + "px";
+      this.style.overflowY = "hidden";
     }
   });
 });
